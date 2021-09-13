17824ef5af148eaa103110f643cafed4fb5e4cab
nikluep <nikluep@t-online.de>
2021-09-13 22:20:23 +0200

    Fixed incorrect function declaration

--- GeneticEvolution/random.h
+++ GeneticEvolution/random.h
@@ -5,6 +5,6 @@
 static std::mt19937 gen(1337);
 static std::uniform_real_distribution<float> distr(-1.f, 1.f);
 
-extern float getRandomFloat() {
+inline float getRandomFloat() {
 	return distr(gen);
 }
